3d4b6a9287ac79b3e3b4b791422df8d015e3629c
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-16 20:39:35 +0200

    Update

--- Test/src/main.cpp
+++ Test/src/main.cpp
@@ -1,26 +1,8 @@
-#include <fstream>
 #include <iostream>
-
-int
-readFromFile ()
-{
-    std::ifstream inputFile ("file.in");
-
-    int x;
-
-    inputFile >> x;
-
-    return x;
-}
 
 int
 main ()
 {
-    int x;
-    std::cout << "Hello World" << std::endl;
-    std::cin >> x;
-    std::cout << x << std::endl;
-
-    std::cout << readFromFile () << std::endl;
+    std::cout << "Szia Apa" << std::endl;
     return 0;
 }
