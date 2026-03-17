a0637d2b0aa439a01c191303f55eb8f0d38fb19f
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-17 09:58:26 +0200

    Update

---
+++ Test/build/CMakeFiles/Test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Test.dir/link.d"
+  "/home/g3nt3lm4n/Code/Test/src/Test"
+  "/home/g3nt3lm4n/Code/Test/src/Test.pdb"
+  "CMakeFiles/Test.dir/src/main.cpp.o"
+  "CMakeFiles/Test.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
