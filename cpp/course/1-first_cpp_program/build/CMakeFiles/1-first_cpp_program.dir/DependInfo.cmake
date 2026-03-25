5da1d57eb20eda5122d9c86e08ddd64a778af187
G3ntl3m4n <urbanpeter08@outlook.hu>
2026-03-25 21:00:44 +0200

    update

---
+++ cpp/course/1-first_cpp_program/build/CMakeFiles/1-first_cpp_program.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/g3nt3lm4n/Code/cpp/course/1-first_cpp_program/src/main.cpp" "CMakeFiles/1-first_cpp_program.dir/src/main.cpp.o" "gcc" "CMakeFiles/1-first_cpp_program.dir/src/main.cpp.o.d"
+  "" "/home/g3nt3lm4n/Code/cpp/course/1-first_cpp_program/src/1-first_cpp_program" "gcc" "CMakeFiles/1-first_cpp_program.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/course/1-first_cpp_program/build/CMakeFiles/1-first_cpp_program.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1-first_cpp_program.dir/link.d"
+  "/home/g3nt3lm4n/Code/cpp/course/1-first_cpp_program/src/1-first_cpp_program"
+  "/home/g3nt3lm4n/Code/cpp/course/1-first_cpp_program/src/1-first_cpp_program.pdb"
+  "CMakeFiles/1-first_cpp_program.dir/src/main.cpp.o"
+  "CMakeFiles/1-first_cpp_program.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1-first_cpp_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
